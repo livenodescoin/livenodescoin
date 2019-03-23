929e48ea1229c3cf510edbaed917d530cc6d86d7
barrystyle <barrystyle@westnet.com.au>
2019-03-24 07:21:27 +0800

    compare tier/reward with data from masternode list, reject fraudelent tx/mn

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,8 @@ map<uint256, uint256> mapProofOfStake;
 set<pair<COutPoint, unsigned int> > setStakeSeen;
 map<unsigned int, unsigned int> mapHashedBlocks;
 map<COutPoint, int> mapStakeSpent;
+std::map<uint256, CAmount> mnTierMap;
+int64_t mnTierCacheTime = GetTime();
 CChain chainActive;
 CBlockIndex* pindexBestHeader = NULL;
 int64_t nTimeBestReceived = 0;
@@ -3213,46 +3215,79 @@ bool CheckBlock(const CBlock& block, CValidationState& state, bool fCheckPOW, bo
                 return state.DoS(100, error("CheckBlock() : more than one coinstake"));
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    if (!IsInitialBlockDownload() && masternodeSync.IsSynced()) {
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (!IsInitialBlockDownload() &&
+        masternodeSync.IsSynced()) {
 
-		int forkHeight = 36500;
-		int blockHeight = chainActive.Height();
+	// flush cache periodically
+	if ((GetTime() - mnTierCacheTime) > 10800) {
+		mnTierMap.clear();
+		mnTierCacheTime = GetTime();
+		LogPrintf("* Flushed mnTier cache\n");
+	}
 
-		if (block.IsProofOfStake() && (blockHeight > forkHeight)) {
+        int blockHeight = chainActive.Height();
+        int payees = block.vtx[1].vout.size() - 2;
 
-				CAmount nCoinbaseRewardTotal = GetBlockValue(blockHeight);
+	if (payees >= 3)
+        {
+                for (int c = 2; c < 5; c++) {
 
-				// find out rewards per tier
-				CAmount nTierRewards[4];
-				for (int b = 1; b < 4; b++)
-					nTierRewards[b] = GetMasternodePayment(blockHeight, b, nCoinbaseRewardTotal);
+			// extract collat info from mn
+			CMasternode* pmn = mnodeman.Find(block.vtx[1].vout[c].scriptPubKey);
+			if (!pmn) {
+				LogPrintf("* Masternode with this vin not found\n");
+				return state.DoS(100, error("CheckBlock() : no match in masternode list for vin"), REJECT_INVALID, "unknown-mn");
+			}
 
-				// test the tx
-				int inwinners = 0;
-				for (int c = 2; c < block.vtx[1].vout.size(); c++) {
+			// if we know this mn, we know the tier amount
+			CAmount nCollateralAmount = 0;
+			uint256 nCollateralHash = pmn->vin.prevout.hash;
+			int nCollateralN = pmn->vin.prevout.n;
 
-					CTxDestination address;
-					ExtractDestination(block.vtx[1].vout[c].scriptPubKey, address);
-					CBitcoinAddress addressMN(address);
+			if (mnTierMap.count(nCollateralHash) > 0) {
+			   for (auto it = mnTierMap.find(nCollateralHash); it != mnTierMap.end(); it++) {
+				if (it->first == nCollateralHash)
+					nCollateralAmount = it->second;
+			   }
+			   LogPrintf("* Found cached entry in local map\n");
+			}
 
-					LogPrintf("* vtx[1].vout[%d] pays to %s - address: %s ", c, block.vtx[1].vout[c].scriptPubKey.ToString().c_str(), addressMN.ToString());
-					LogPrintf("- %llu (%d)\n", block.vtx[1].vout[c].nValue, (block.vtx[1].vout[c].nValue == nTierRewards[c-1]));
+			// if it wasnt in our map/cache
+			if (nCollateralAmount == 0) {
 
-					// see if the address is in the winners list
-					if (mnodeman.Find(block.vtx[1].vout[c].scriptPubKey))
-						inwinners++;
+				LogPrintf("* Retrieving collateral transaction from disk\n");
+
+				uint256 blockHash;
+				CTransaction nCollateralTx;
+				if (!GetTransaction(nCollateralHash, nCollateralTx, blockHash, true))
+				    return state.DoS(100, error("CheckBlock() : could not find collateral transaction for masternode"), REJECT_INVALID, "unknown-mn");
+				CAmount potential = nCollateralTx.vout[nCollateralN].nValue;
+				if (potential == 1000 * COIN || potential == 2000 * COIN || potential == 5000 * COIN) {
+				    nCollateralAmount = potential;
+				    mnTierMap.insert(std::pair<uint256, CAmount>(nCollateralHash, nCollateralAmount));
+				    LogPrintf("* Added (%s,%llu) to mnTierMap\n", nCollateralHash.ToString().c_str(), nCollateralAmount);
 				}
+			}
 
-				if(inwinners == 3) {
-					LogPrintf("All addresses were found to be in the masternode winners list\n");
-				} else {
-					LogPrintf("At least one address has been injected as a block winner\n");
-					return state.DoS(100, error("CheckBlock() : at least one address isn't in winners list"), REJECT_INVALID, "unknown-mn-winner");
-				}
+			// match mn/tier to a known reward
+			int nTier = 0;
+			if (nCollateralAmount == 1000 * COIN) nTier = 1;
+			if (nCollateralAmount == 2000 * COIN) nTier = 2;
+			if (nCollateralAmount == 5000 * COIN) nTier = 3;
+
+			CAmount nBlockValue = GetBlockValue(blockHeight);
+			CAmount nTierReward = GetMasternodePayment(blockHeight, (c-1), nBlockValue);
+			if (nTierReward == block.vtx[1].vout[c].nValue && (nTier == c - 1)) {
+				LogPrintf("* Tier %d matched to correct Masternode\n", (c-1));
+			} else {
+				LogPrintf("* Tier %d matched to invalid Masternode\n", (c-1));
+				return state.DoS(100, error("CheckBlock() : masternode doesnt belong to this payment tier"), REJECT_INVALID, "fraudulent-mn");
+			}
 		}
+        }
     }
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // ----------- swiftTX transaction scanning -----------
     if (IsSporkActive(SPORK_2_SWIFTTX_BLOCK_FILTERING)) {
